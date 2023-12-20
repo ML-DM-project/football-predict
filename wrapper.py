b5a5d739020b34f255daf6c6c3883255a55fb3c0
huydt84 <viethuy061002@gmail.com>
2023-12-20 11:23:16 +0700

    finish feature selection

--- wrapper.py
+++ wrapper.py
@@ -49,20 +49,25 @@ def process_data(data_path: str,
     elif fill_na == "mean":
         X_train.fillna(X_train.mean(), inplace=True)
         X_test.fillna(X_test.mean(), inplace=True)
-    elif fill_na == "auto":   # fill based on feature engineering
-        X_train['homeAvgRating'] = X_train['homeAvgRating'].fillna(X_train['homeAvgRating'].mean())
-        X_train['homePoint'] = X_train['homePoint'].fillna(X_train['homePoint'].median())
-        X_train['homeForm'] = X_train['homeForm'].fillna(X_train['homeForm'].mean())
-        X_train['awayAvgRating'] = X_train['awayAvgRating'].fillna(X_train['awayAvgRating'].mean())
-        X_train['awayPoint'] = X_train['awayPoint'].fillna(X_train['awayPoint'].median())
-        X_train['awayForm'] = X_train['awayForm'].fillna(X_train['awayForm'].mean())
-        
-        X_test['homeAvgRating'] = X_test['homeAvgRating'].fillna(X_test['homeAvgRating'].mean())
-        X_test['homePoint'] = X_test['homePoint'].fillna(X_test['homePoint'].median())
-        X_test['homeForm'] = X_test['homeForm'].fillna(X_test['homeForm'].mean())
-        X_test['awayAvgRating'] = X_test['awayAvgRating'].fillna(X_test['awayAvgRating'].mean())
-        X_test['awayPoint'] = X_test['awayPoint'].fillna(X_test['awayPoint'].median())
-        X_test['awayForm'] = X_test['awayForm'].fillna(X_test['awayForm'].mean())
+    elif fill_na == "auto": # fill based on feature engineering
+        if 'homeAvgRating' in list_column:
+            X_train['homeAvgRating'] = X_train['homeAvgRating'].fillna(X_train['homeAvgRating'].mean())
+            X_test['homeAvgRating'] = X_test['homeAvgRating'].fillna(X_test['homeAvgRating'].mean())
+        if 'homePoint' in list_column:
+            X_train['homePoint'] = X_train['homePoint'].fillna(X_train['homePoint'].median())
+            X_test['homePoint'] = X_test['homePoint'].fillna(X_test['homePoint'].median())
+        if 'homeForm' in list_column:
+            X_train['homeForm'] = X_train['homeForm'].fillna(X_train['homeForm'].mean())
+            X_test['homeForm'] = X_test['homeForm'].fillna(X_test['homeForm'].mean())
+        if 'awayAvgRating' in list_column:
+            X_train['awayAvgRating'] = X_train['awayAvgRating'].fillna(X_train['awayAvgRating'].mean())
+            X_test['awayAvgRating'] = X_test['awayAvgRating'].fillna(X_test['awayAvgRating'].mean())
+        if 'awayPoint' in list_column:
+            X_train['awayPoint'] = X_train['awayPoint'].fillna(X_train['awayPoint'].median())
+            X_test['awayPoint'] = X_test['awayPoint'].fillna(X_test['awayPoint'].median())
+        if 'awayForm' in list_column:
+            X_train['awayForm'] = X_train['awayForm'].fillna(X_train['awayForm'].mean())
+            X_test['awayForm'] = X_test['awayForm'].fillna(X_test['awayForm'].mean())
         
 
     if scaler == "standard":
@@ -92,6 +97,33 @@ def lazy_feature_selection(data_path: str,
                             fill_na: Literal[0, "median", "mean", "auto"] = "auto",
                            scaler: Literal["standard", "minmax", "maxabs", "normal", "power", "robust", "quantile"] = "standard"):
     sc, X_train, X_test, y_train, y_test = process_data(data_path, list_column, fill_na, scaler)
+    clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
+    score, predictions = clf.fit(X_train, X_test, y_train, y_test)
+    
+    return score
+
+def lazy_train(X_train, X_test, y_train, y_test, 
+               scaler: Literal["standard", "minmax", "maxabs", "normal", "power", "robust", "quantile"] = "quantile"):
+    if scaler == "standard":
+        sc = StandardScaler()
+    elif scaler == "minmax":
+        sc = MinMaxScaler()
+    elif scaler == "maxabs":
+        sc = MaxAbsScaler()
+    elif scaler == "normal":
+        sc = Normalizer()
+    elif scaler == "power":
+        sc = PowerTransformer()
+    elif scaler == "robust":
+        sc = RobustScaler()
+    elif scaler == "quantile":
+        sc = QuantileTransformer()
+    else:
+        raise ValueError
+    
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    
     clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
     score, predictions = clf.fit(X_train, X_test, y_train, y_test)
     
